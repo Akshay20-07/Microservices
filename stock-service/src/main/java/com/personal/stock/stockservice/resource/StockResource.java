4ed76b82e41044444f91206e8a901bcc28adc281
ezaksch <akshaychauhanjp@gmail.com>
2020-03-15 15:25:23 +0530

    registered with eureka

--- stock-service/src/main/java/com/personal/stock/stockservice/resource/StockResource.java
+++ stock-service/src/main/java/com/personal/stock/stockservice/resource/StockResource.java
@@ -1,6 +1,7 @@
 package com.personal.stock.stockservice.resource;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -16,7 +17,6 @@ import org.springframework.web.client.RestTemplate;
 
 import yahoofinance.Stock;
 import yahoofinance.YahooFinance;
-import yahoofinance.quotes.stock.StockQuote;
 
 /**
  * To query DB service use Rest Template 
@@ -31,10 +31,15 @@ public class StockResource {
 	@Autowired
 	RestTemplate restTemp;
 	
+	/**
+	 * To retrieve the Stock
+	 * @param userName
+	 * @return
+	 */
 	@GetMapping("/{username}")
 	public List<Stock> getStock(@PathVariable("username") final String userName){
 			
-		ResponseEntity<List<String>> quoteResponse=restTemp.exchange("http://localhost:8081/rest/db/"+userName, HttpMethod.GET, null, new ParameterizedTypeReference<List<String>>() {
+		ResponseEntity<List<String>> quoteResponse=restTemp.exchange("http://db-service/rest/db/"+userName, HttpMethod.GET, null, new ParameterizedTypeReference<List<String>>() {
 		});
 		
 		List<String> quotes=quoteResponse.getBody();
@@ -44,6 +49,53 @@ public class StockResource {
 					.collect(Collectors.toList());
 	}
 	
+	/**
+	 * For fetching the Currency of the Stock
+	 * @param userName
+	 * @return
+	 */
+	@GetMapping("/{username}/currency")
+	public List<String> getStockCurrency(@PathVariable("username") final String userName){
+			
+		ResponseEntity<List<String>> quoteResponse=restTemp.exchange("http://db-service/rest/db/"+userName, HttpMethod.GET, null, new ParameterizedTypeReference<List<String>>() {
+		});
+		
+		List<String> quotes=quoteResponse.getBody();
+		return quotes
+				.stream()
+					.map(this::getStockCurrencyName)
+					.collect(Collectors.toList());
+	}
+	
+	
+	
+	
+	/**
+	 * To get the details of the User
+	 * @param userName
+	 * @return
+	 */
+	@GetMapping("/{username}/details")
+	public List<List<Object>> getUserDetails(@PathVariable("username")  final String userName){
+		ResponseEntity<List<String>> quoteResponse=restTemp.exchange("http://db-service/rest/db/"+userName, HttpMethod.GET,null,new ParameterizedTypeReference<List<String>>() {
+		});
+		
+		List<String> quotes=quoteResponse.getBody();
+		
+		
+		return quotes
+				.stream()
+				.map(this::getDetails)
+				.collect(Collectors.toList());
+		
+	}
+	
+	
+	/**
+	 * To get the Current Price of the Stock
+	 * @param quote
+	 * @return
+	 */
 	private Stock getStockPrice(String quote){
 		try {
 			return YahooFinance.get(quote);
@@ -53,8 +105,44 @@ public class StockResource {
 			return new Stock(quote);
 		}
 		
+	}
+	
+	/**To get the Currency of the stock
+	 * @param quote
+	 * @return
+	 */
+	private String getStockCurrencyName(String quote){
+		try {
+			return YahooFinance.get(quote).getCurrency();
+		} catch (IOException e) {
+			
+			e.printStackTrace();
+			return null;
+		}
+	
+	}
+	/**
+	 * To retrieve the StockExchange name,Dividend and statistics
+	 * @param quote1
+	 * @return
+	 */
+	private List<Object> getDetails(String quote1){
+		List<Object> detail=new ArrayList<>();
+		Stock stock;
+		try {
+			stock = YahooFinance.get(quote1);
+		} catch (IOException e) {
+			
+			e.printStackTrace();
+			stock=new Stock(quote1);
+		}
+		detail.add(stock.getStockExchange());
+		detail.add(stock.getDividend());
+		detail.add(stock.getStats());
+		
+		return detail;
+	}
 		
 		
 	}
 
-}
--- stock-service/src/main/java/com/personal/stock/stockservice/resource/config/Config.java
+++ stock-service/src/main/java/com/personal/stock/stockservice/resource/config/Config.java
@@ -1,5 +1,6 @@
 package com.personal.stock.stockservice.resource.config;
 
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
@@ -11,7 +12,7 @@ public class Config {
 	 * To create the restTemplate only once
 	 * @return
 	 */
-	//@LoadBalanced
+	@LoadBalanced
 	@Bean
 	public RestTemplate restTemplate(){
 		return new RestTemplate();
